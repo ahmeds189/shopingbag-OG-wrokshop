b87a959f231ba4911b8fcf5dbf503f7aebc3eaf8
ahmed <primaryahmedsaid@gmail.com>
2023-05-06 08:10:54 +0300

    initial header

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import { categories } from './constants'
 import Tooltip from './components/tooltip/Tooltip'
+import Header from './components/header/Header'
 
 const App = () => {
   return (
-    <div className="container">
-      {categories.map((cat) => {
-        const Icon = cat.icon
-        return (
-          <div key={cat.id}>
-            <span>{cat.name}</span>
-            <Icon />
-          </div>
-        )
-      })}
-      <Tooltip title="hot" color="blue" />
-      <Tooltip title="new" color="gold" />
-      <Tooltip title="hot" color="dark-gold" />
-      <Tooltip title="new" color="dark-gold" notRound />
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        {categories.map((cat) => {
+          const Icon = cat.icon
+          return (
+            <div key={cat.id}>
+              <span>{cat.name}</span>
+              <Icon />
+            </div>
+          )
+        })}
+        <Tooltip title="hot" color="blue" />
+        <Tooltip title="new" color="gold" />
+        <Tooltip title="hot" color="dark-gold" />
+        <Tooltip title="new" color="dark-gold" notRound />
+      </div>
+    </>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import { navbarLinks } from '../../constants'
+import { TiArrowSortedDown } from 'react-icons/ti'
+import { MdOutlineSearch } from 'react-icons/md'
+import './header.scss'
 
 const Header = () => {
-  return <header>Navbar</header>
+  return (
+    <header>
+      <div className="container">
+        <nav>
+          <ul>
+            <li>
+              $USD <TiArrowSortedDown />
+            </li>
+            <li className="full">
+              English <TiArrowSortedDown />
+            </li>
+            {navbarLinks.map((link, i) => (
+              <li key={i}>{link}</li>
+            ))}
+          </ul>
+        </nav>
+
+        <div className="search">
+          <h1>
+            shoping<span>bag</span>
+          </h1>
+          <div>
+            <li>
+              Categories <TiArrowSortedDown />
+            </li>
+            <input type="text" placeholder="Search Here" />
+            <MdOutlineSearch size={25} />
+          </div>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
